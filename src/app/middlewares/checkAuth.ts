db016d72a41c911563f9837ac8caaf0c9b7380e0
emonpappu17 <emon.pappu.5@gmail.com>
2025-10-01 23:36:43 +0600

    add: blog schema

--- src/app/middlewares/checkAuth.ts
+++ src/app/middlewares/checkAuth.ts
@@ -8,10 +8,10 @@ import { envVars } from "../config/env";
 
 export const checkAuth = (...authRoles: string[]) => async (req: Request, res: Response, next: NextFunction) => {
     try {
-        console.log("req ===>",req.cookies);
+        // console.log("req ===>",req.cookies);
         const accessToken = req.headers.authorization || req.cookies.accessToken;
 
-        console.log('accessToken==>', accessToken);
+        // console.log('accessToken==>', accessToken);
 
         if (!accessToken) throw new AppError(403, "No Token Received");
 
